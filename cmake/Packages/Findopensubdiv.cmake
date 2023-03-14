568137041a7f48ec312b5261c0bae3cc414caf82
callym <hi@callym.com>
2023-03-14 00:25:51 +0000

    Fix OSD overwriting options

--- cmake/Packages/Findopensubdiv.cmake
+++ cmake/Packages/Findopensubdiv.cmake
@@ -1,10 +1,10 @@
-set(NO_DOC ON)
-set(NO_EXAMPLES ON)
-set(NO_TUTORIALS ON)
-set(NO_REGRESSION ON)
-set(BUILD_SHARED_LIBS OFF)
+set(NO_DOC ON CACHE INTERNAL "")
+set(NO_EXAMPLES ON CACHE INTERNAL "")
+set(NO_TUTORIALS ON CACHE INTERNAL "")
+set(NO_REGRESSION ON CACHE INTERNAL "")
+set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "")
 
-set(OSD_CUDA_NVCC_FLAGS "--gpu-architecture native")
+set(OSD_CUDA_NVCC_FLAGS "--gpu-architecture native" CACHE INTERNAL "")
 
 git(opensubdiv Logarithmus/OpenSubdiv tbb-2021)
 
